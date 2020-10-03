9e3d416981544c763af09141561f3d7d54c92dd4
Ziyang Jiang <red@ziyangs-mbp.wireless-1x.unc.edu>
2020-10-03 16:14:05 -0400

    heatmap setup

--- bundle.js
+++ bundle.js
@@ -1,17 +1,20 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+/** get the map projection function from node module. */
 let proj = require('geo-albers-usa-territories')
 
+/** get the state FIPS code.*/
 let stateFIPS = {};
 fetch('stateFIPS.json')
   .then(resp => resp.json())
   .then(json => stateFIPS = json);
 
+/** set up map scale.*/
 // let devicePixelRatio = window.devicePixelRatio || 1;
 let devicePixelRatio = 1;
 let width = 950 * devicePixelRatio;
 let height = 530 * devicePixelRatio;
 
-
+/** append the svg map.*/
 let svg = d3
   .select('#usmap')
   .append('svg')
@@ -21,35 +24,40 @@ let svg = d3
   .style('left', '0px')
   .style('top', '0px')
 
+
+/** set up map projection.*/
 let projection = proj.geoAlbersUsaTerritories();
 let path = d3.geoPath().projection(projection);
+/** set up color scale */
+
+let sequentialScale = d3.scaleSequential()
+  .domain([0, 1])
+  .interpolator(d3.interpolateOranges);
+
+let testList = [];
+for (let state in stateFIPS) {
+  testList.push(Math.random())
+}
 
 
-
-
-
-d3.json('us_county_5m_topojson.json').then(function (us) {
+d3.json('cb_2019_us_state_5m.json').then(function (us) {
 
   /**
      * This part draws state boundaries.
      */
 
-  for (let state in stateFIPS) {
-    svg.append('path')
-      .datum(topojson.merge(us, us.objects.cb_2019_us_county_5m.geometries.filter(d => { return d.properties.STATEFP === state })))
-      .attr('class', 'state-boundary')
-      .attr('d', path)
-      .attr('state', stateFIPS[state])
+  // for (let state in stateFIPS) {
+  //   svg.append('path')
+  //     .datum(topojson.merge(us, us.objects.cb_2019_us_county_5m.geometries.filter(d => { return d.properties.STATEFP === state })))
+  //     .attr('class', 'state')
+  //     .attr('d', path)
+  //     .attr('state', stateFIPS[state])
+  //     .on('mouseenter', mouseEnter)
+  //     .on('mouseleave', mouseLeave)
+  //     .on('mousemove', mouseMove)
 
-  }
+  // }
 
-  let state = document.querySelectorAll('.state-boundary');
-
-  for (let s of state) {
-    s.addEventListener('mouseenter', () => {
-      console.log(s.attributes.state.value)
-    })
-  }
 
 
   /**
@@ -57,43 +65,53 @@ d3.json('us_county_5m_topojson.json').then(function (us) {
    */
 
   svg.selectAll(".region")
-    .data(topojson.feature(us, us.objects.cb_2019_us_county_5m).features.filter(d => d.properties.STATEFP == 02))
+    .data(topojson.feature(us, us.objects.cb_2019_us_state_5m).features)
     .enter()
     .append("path")
-    .attr("class", "county")
+    .attr("class", "state")
     .attr("d", path)
     .attr("name", d => d.properties.NAME)
-    .style("fill", "#aca")
-    .style("stroke", "#fff")
-    .style("stroke-width", "0.2px")
-    .on('mousedown', (d) => { console.log(d.properties.NAME) })
+    .on('mouseenter', mouseEnter)
+    .on('mouseleave', mouseLeave)
+    .on('mousemove', mouseMove)
 
-  /**
-   * This part draws the map as a whole.
-   */
-
-  // svg.append('path')
-  //   .datum(topojson.feature(us, us.objects.cb_2019_us_county_5m))
-  //   .attr('class', 'state')
-  //   .attr('d', path);
-
-
-
-  /**
-   * This part draws counties.
-   */
-
-  // svg.append("path")
-  //   .datum(topojson.mesh(us, us.objects.cb_2019_us_county_5m, function (a, b) { return a !== b; }))
-  //   .attr("class", "county-boundary")
-  //   .attr("d", path);
-
-
-
-
-
+  d3.selectAll('path.state').each(function () {
+    d3.select(this).style('fill', sequentialScale(Math.random()))
+  })
 })
 
+
+
+let mouseEnter = function (d) {
+  d3.selectAll(".state")
+    .style("opacity", .5)
+  d3.select(this)
+    .style("opacity", 1)
+    .style("stroke-width", "1.5px")
+  d3.select('.tooltip')
+    .style('opacity', 1)
+}
+
+let mouseLeave = function (d) {
+  d3.selectAll(".state")
+    .style("opacity", 1)
+  d3.select(this)
+    .style("stroke-width", "0.5px")
+  d3.select('.tooltip')
+    .style('opacity', 0)
+}
+
+let mouseMove = function (d) {
+  d3.select('.tooltip')
+    .html(d3.select(this).attr('name'))
+    .style("left", (d3.mouse(this)[0] + 30) + "px")
+    .style("top", (d3.mouse(this)[1]) + "px")
+}
+
+let toolTip = d3.select("#usmap")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0)
 
 
 
--- main.js
+++ main.js
@@ -24,32 +24,38 @@ let svg = d3
   .style('top', '0px')
 
 
-/** draw the map.*/
+/** set up map projection.*/
 let projection = proj.geoAlbersUsaTerritories();
 let path = d3.geoPath().projection(projection);
+/** set up color scale */
 
-d3.json('us_county_5m_topojson.json').then(function (us) {
+let sequentialScale = d3.scaleSequential()
+  .domain([0, 1])
+  .interpolator(d3.interpolateOranges);
+
+let testList = [];
+for (let state in stateFIPS) {
+  testList.push(Math.random())
+}
+
+
+d3.json('cb_2019_us_state_5m.json').then(function (us) {
 
   /**
      * This part draws state boundaries.
      */
 
-  for (let state in stateFIPS) {
-    svg.append('path')
-      .datum(topojson.merge(us, us.objects.cb_2019_us_county_5m.geometries.filter(d => { return d.properties.STATEFP === state })))
-      .attr('class', 'state-boundary')
-      .attr('d', path)
-      .attr('state', stateFIPS[state])
+  // for (let state in stateFIPS) {
+  //   svg.append('path')
+  //     .datum(topojson.merge(us, us.objects.cb_2019_us_county_5m.geometries.filter(d => { return d.properties.STATEFP === state })))
+  //     .attr('class', 'state')
+  //     .attr('d', path)
+  //     .attr('state', stateFIPS[state])
+  //     .on('mouseenter', mouseEnter)
+  //     .on('mouseleave', mouseLeave)
+  //     .on('mousemove', mouseMove)
 
-  }
-
-  let state = document.querySelectorAll('.state-boundary');
-
-  for (let s of state) {
-    s.addEventListener('mouseenter', () => {
-      console.log(s.attributes.state.value)
-    })
-  }
+  // }
 
 
 
@@ -58,29 +64,53 @@ d3.json('us_county_5m_topojson.json').then(function (us) {
    */
 
   svg.selectAll(".region")
-    .data(topojson.feature(us, us.objects.cb_2019_us_county_5m).features.filter(d => d.properties.STATEFP == 02))
+    .data(topojson.feature(us, us.objects.cb_2019_us_state_5m).features)
     .enter()
     .append("path")
-    .attr("class", "county")
+    .attr("class", "state")
     .attr("d", path)
     .attr("name", d => d.properties.NAME)
-    .on('mousedown', (d) => { console.log(d.properties.NAME) })
+    .on('mouseenter', mouseEnter)
+    .on('mouseleave', mouseLeave)
+    .on('mousemove', mouseMove)
 
-
-
-
-  /**
-   * This part draws the map as a whole.
-   */
-
-  // svg.append('path')
-  //   .datum(topojson.feature(us, us.objects.cb_2019_us_county_5m))
-  //   .attr('class', 'state')
-  //   .attr('d', path);
-
-
+  d3.selectAll('path.state').each(function () {
+    d3.select(this).style('fill', sequentialScale(Math.random()))
+  })
 })
 
+
+
+let mouseEnter = function (d) {
+  d3.selectAll(".state")
+    .style("opacity", .5)
+  d3.select(this)
+    .style("opacity", 1)
+    .style("stroke-width", "1.5px")
+  d3.select('.tooltip')
+    .style('opacity', 1)
+}
+
+let mouseLeave = function (d) {
+  d3.selectAll(".state")
+    .style("opacity", 1)
+  d3.select(this)
+    .style("stroke-width", "0.5px")
+  d3.select('.tooltip')
+    .style('opacity', 0)
+}
+
+let mouseMove = function (d) {
+  d3.select('.tooltip')
+    .html(d3.select(this).attr('name'))
+    .style("left", (d3.mouse(this)[0] + 30) + "px")
+    .style("top", (d3.mouse(this)[1]) + "px")
+}
+
+let toolTip = d3.select("#usmap")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0)
 
 
 
